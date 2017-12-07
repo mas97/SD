4c3e5bca8c9d6e83fa9a375055d2de8acf55e5ec
Marco Silva <MarcoSilva@MarcoSilva.local>
2017-12-07 10:54:22 +0000

    Adição na classe Jogador do método para verificação da palavra passe e construtores parametrizado e vazio.

--- Overwatch/src/overwatch/Jogador.java
+++ Overwatch/src/overwatch/Jogador.java
@@ -10,5 +10,31 @@ package overwatch;
  * @author MarcoSilva
  */
 public class Jogador {
-    
+
+    /* Variáveis de instância */
+    private String username;
+    private String password;
+
+    /* Construtor vazio */
+    public Jogador() {
+        this.username = "NA";
+        this.password = "NA";//colocamos caracteres aleatórios para segunrança??
+    }
+
+    /* Construtor parametrizado */
+    public Jogador(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    /* Método privado apenas utilizado nesta classe no método checkPassword */
+    private String getPassword() {
+        return this.password;
+    }
+
+    /* Método utilizado para verificação da password de um Jogador */
+    public boolean checkPassword(String password) {
+        return this.getPassword().equals(password);
+    }
+
 }
