0ad4c788f3004de7e3e8d59040743ac237f8c5f5
afonscosta <afonso.costa.15@gmail.com>
2017-12-31 12:43:16 +0000

    scripts para compilar o trabalho no terminal

---
+++ afonso.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cp -r /home/rokai/Dropbox/1º\ Semestre/4_SD/4_Projeto/mas97/Paladins /home/rokai/Documents/
+
+cd /home/rokai/Documents/Paladins/src/paladins/
+
+javac -d . ChatEscolhaHerois.java FazEquipas.java HashChats.java HashFazEquipas.java HashJogos.java HashTimers.java Heroi.java JogadoresInscritos.java Jogador.java Jogo.java Matchmaking.java Partida.java ServerWorker.java Servidor.java Timer.java TrataJogadorEscrita.java TrataJogadorLeitura.java
+
+javac -d . Cliente.java ClientWorker.java
+
+echo "Servidor a começar..."
+
+java paladins.Servidor
