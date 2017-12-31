9512d3fc404c997c8ed2ef36dc63b7bc351029c4
Marco Silva <MarcoSilva@MarcoSilva.local>
2017-12-31 18:51:30 +0000

    Adição da classe responsável por correr os testes e alteração da classe servidor para Runnable

--- Paladins/src/paladins/Servidor.java
+++ Paladins/src/paladins/Servidor.java
@@ -15,71 +15,67 @@ import java.util.Map;
  *
  * @author MarcoSilva
  */
-public class Servidor {
+public class Servidor implements Runnable {
 
+    @Override
+    public void run() {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+        // Map que dado um rank (0-8) corresponde a respetiva sala de matchmaking
+        HashMap<Integer, Matchmaking> salasRank = new HashMap<>();
+        //Cria as 9 salas para fazer o matchmaking
+        for (int i = 0; i < 9; i++) {
+            salasRank.put(i, new Matchmaking(2));
+        }
 
-		// Map que dado um rank (0-8) corresponde a respetiva sala de matchmaking
-		HashMap<Integer, Matchmaking> salasRank = new HashMap<>();
-		//Cria as 9 salas para fazer o matchmaking
-		for (int i = 0; i < 9; i++)	
-			salasRank.put(i, new Matchmaking(2));
-		
-		// Hash com os dados necessários para criar uma equipa
-		HashFazEquipas hashFE = new HashFazEquipas();
-		
-		// Hash com os dados necessários para criar um chat
-		HashChats hashC = new HashChats();
-                
-		// Hash com os timers para cada uma das salas
-		HashTimers timers = new HashTimers();
-		
-		// Hash com os jogos para cada uma das partidas
-		HashJogos hashJ = new HashJogos();
-		
-		// Hash com o registo de todos os jogadores
-		HashMap<String, Jogador> jogadoresHash = new HashMap<>();
-		JogadoresInscritos jogadores = new JogadoresInscritos(jogadoresHash);
-		
-		
-		//HashMap com os 30 heróis
-		HashMap<String, Heroi> herois = new HashMap<>();
-		herois.put("Barik", new Heroi("Barik"));
-		herois.put("Buck", new Heroi("Buck"));
-		herois.put("Cassie", new Heroi("Cassie"));
-		herois.put("Fernando", new Heroi("Fernando"));
-		herois.put("Grohk", new Heroi("Grohk"));
-		herois.put("Pip", new Heroi("Pip"));
-		herois.put("Ruckus", new Heroi("Ruckus"));
-		herois.put("Skye", new Heroi("Skye"));
-		herois.put("Evie", new Heroi("Evie"));
-		herois.put("Grover", new Heroi("Grover"));
-		herois.put("Kinessa", new Heroi("Kinessa"));
-		herois.put("Androxus", new Heroi("Androxus"));
-		herois.put("Ying", new Heroi("Ying"));
-		herois.put("Drogoz", new Heroi("Drogoz"));
-		herois.put("Bomb King", new Heroi("Bomb King"));
-		herois.put("Viktor", new Heroi("Viktor"));
-		herois.put("Makoa", new Heroi("Makoa"));
-		herois.put("Mal'Damba", new Heroi("Mal'Damba"));
-		herois.put("Sha Lin", new Heroi("Sha Lin"));
-		herois.put("Tyra", new Heroi("Tyra"));
-		herois.put("Torvald", new Heroi("Torvald"));
-		herois.put("Maeve", new Heroi("Maeve"));
-		herois.put("Inara", new Heroi("Inara"));
-		herois.put("Lex", new Heroi("Lex"));
-		herois.put("Seris", new Heroi("Seris"));
-		herois.put("Willo", new Heroi("Willo"));
-		herois.put("Ash", new Heroi("Ash"));
-		herois.put("Zhin", new Heroi("Zhin"));
-		herois.put("Lian", new Heroi("Lian"));
-		herois.put("Jenos", new Heroi("Jenos"));
-		
-		
+        // Hash com os dados necessários para criar uma equipa
+        HashFazEquipas hashFE = new HashFazEquipas();
+
+        // Hash com os dados necessários para criar um chat
+        HashChats hashC = new HashChats();
+
+        // Hash com os timers para cada uma das salas
+        HashTimers timers = new HashTimers();
+
+        // Hash com os jogos para cada uma das partidas
+        HashJogos hashJ = new HashJogos();
+
+        // Hash com o registo de todos os jogadores
+        HashMap<String, Jogador> jogadoresHash = new HashMap<>();
+        JogadoresInscritos jogadores = new JogadoresInscritos(jogadoresHash);
+
+        //HashMap com os 30 heróis
+        HashMap<String, Heroi> herois = new HashMap<>();
+        herois.put("Barik", new Heroi("Barik"));
+        herois.put("Buck", new Heroi("Buck"));
+        herois.put("Cassie", new Heroi("Cassie"));
+        herois.put("Fernando", new Heroi("Fernando"));
+        herois.put("Grohk", new Heroi("Grohk"));
+        herois.put("Pip", new Heroi("Pip"));
+        herois.put("Ruckus", new Heroi("Ruckus"));
+        herois.put("Skye", new Heroi("Skye"));
+        herois.put("Evie", new Heroi("Evie"));
+        herois.put("Grover", new Heroi("Grover"));
+        herois.put("Kinessa", new Heroi("Kinessa"));
+        herois.put("Androxus", new Heroi("Androxus"));
+        herois.put("Ying", new Heroi("Ying"));
+        herois.put("Drogoz", new Heroi("Drogoz"));
+        herois.put("Bomb King", new Heroi("Bomb King"));
+        herois.put("Viktor", new Heroi("Viktor"));
+        herois.put("Makoa", new Heroi("Makoa"));
+        herois.put("Mal'Damba", new Heroi("Mal'Damba"));
+        herois.put("Sha Lin", new Heroi("Sha Lin"));
+        herois.put("Tyra", new Heroi("Tyra"));
+        herois.put("Torvald", new Heroi("Torvald"));
+        herois.put("Maeve", new Heroi("Maeve"));
+        herois.put("Inara", new Heroi("Inara"));
+        herois.put("Lex", new Heroi("Lex"));
+        herois.put("Seris", new Heroi("Seris"));
+        herois.put("Willo", new Heroi("Willo"));
+        herois.put("Ash", new Heroi("Ash"));
+        herois.put("Zhin", new Heroi("Zhin"));
+        herois.put("Lian", new Heroi("Lian"));
+        herois.put("Jenos", new Heroi("Jenos"));
+
         try {
             ServerSocket sSocket = new ServerSocket(12345);
             while (true) {
---
+++ Paladins/src/paladins/TestClass.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paladins;
+
+/**
+ *
+ * @author MarcoSilva
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Iniciando o servidor de jogo...");
+        Thread server = new Thread(new Servidor());
+        server.start();
+
+        NameGenerator gen = new NameGenerator();
+
+        for (int i = 0; i < 100; i++) {
+            String username = gen.getName();
+            String password = gen.getName();
+
+            System.out.println("Jogador criado com o username " + username + " e com a password " + password);
+        }
+    }
+}
